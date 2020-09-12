5c9ec73955ba829fd05dd1b1ee2a5dc0a3c756f1
Tanbir Hasan <tanbir2025@gmail.com>
2020-09-12 17:58:33 +0600

    fix name

--- database/migrations/2020_09_12_110956_create_posts_table.php
+++ database/migrations/2020_09_12_110956_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('text');
+            $table->string('title');
             $table->text('description');
             $table->string('image');
             $table->timestamps();
